4934c775b899cc1fb275c7becbfd95cfc40d1a9c
Abhay Mathur <a21.mathur21@gmail.com>
2024-03-30 15:32:06 +0530

    person apis ready

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pymongo.mongo_client import MongoClient
 from bson import json_util
 import json
+from chatbot import llm_model
 
 app = Flask(__name__)
 
@@ -11,35 +11,122 @@ uri = "mongodb+srv://abhaymathur21:itsmeright@codeshastra.lxorakw.mongodb.net/?r
 # Create a new client and connect to the server
 client = MongoClient(uri)
 db = client["Accounts"]
-
     
-@app.route('/', methods=['GET','POST'])
-def index():
+@app.route("/add_person", methods=['POST'])
+def add_person():
     
-    # Send a ping to confirm a successful connection
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-    except Exception as e:
-        print(e)
-        # Example: Retrieve documents from a collection
-    collection = db["User1"]
+    document = request.get_json()
+    
+    # Create a new collection or get the existing collection
+    collection = db["User"+document["userID"]]
     # documents = collection.find({})
     # result = [json.loads(json_util.dumps(doc)) for doc in documents]  # Convert ObjectId to string
     # print(result)
     
-    document = {
-        "userID": 1,
-        "userName": "Abhay Mathur",
-        "chatHistory": "example_text",
-        "modelName": "example_model.h5"
-    }
-
     # Insert the document into the collection
     collection.insert_one(document)
     
     message = "Document inserted successfully."
+    
+    # Example Input:
+    # {
+    #     "personID": "1",
+    #     "personName": "person1",
+    #     "userID": "1",
+    #     "userName": "User1",
+    #     "chatHistory": "chat_history",
+    #     "modelName": "model_name.h5"
+    # }
+    
+    print(message)
     return message
+
+
+@app.route("/update_person", methods=['POST'])
+def update_person():
+    
+    data = request.get_json()
+    
+    collection = db["User"+data["userID"]]
+    # Find the document to update
+    
+    query = {"personID": data["personID"]}
+    
+    document = collection.find_one(query)
+    print(document)
+    if document:
+        # Update the fields of the document
+        document["personName"] = data["personName"] if data["personName"] else document["personName"]
+        document["chatHistory"] = data["chatHistory"] if data["chatHistory"] else document["chatHistory"]
+        document["modelName"] = data["modelName"] if data["modelName"] else document["modelName"]
+
+        document = {
+            "$set": document
+        }
+        
+        # Save the updated document
+        collection.update_one(query, document)
+        
+        message = "Document updated successfully."
+
+        
+    else:
+        message = "Document not found."
+        
+    # Example input: 
+    # {
+    #     "personID": "1",
+    #     "personName": "",
+    #     "userID": "1",
+    #     "chatHistory": "updated_chat_history",
+    #     "modelName": ""
+
+    # }    
+    
+    print(message)
+    return message
+    
+    
+@app.route("/delete_person", methods=['POST'])
+def delete_person():
+    
+    data = request.get_json()
+    
+    collection = db["User"+data["userID"]]
+    # Find the document to delete
+    document = collection.find_one({"personID": data["personID"]})
+    
+    if document:
+        # Delete the document
+        collection.delete_one({"personID": data["personID"]})
+        
+        message = "Document deleted successfully."
+    else:
+        message = "Document not found."
+        
+    # Example input:
+    # {
+    #     "personID": "1",
+    #     "userID": "1"
+    # }
+    
+    print(message)
+    return message
+    
+
+@app.route("/llm_chatbot", methods=['GET', 'POST'])
+def llm_chatbot():
+    
+    if request.method == "POST":
+        data = request.get_json()
+        input_string = data.get('message')
+        
+        print('Received string:', input_string)
+        
+        response = llm_model(input_string)
+        # print(response)
+        return response
+
 
 
 if __name__ == '__main__':
---
+++ backend/functionalities/pdf_to_image.py
@@ -0,0 +1,84 @@
+import fitz  # PyMuPDF
+import google.generativeai as genai
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+if "GOOGLE_API_KEY" not in os.environ:
+    raise ValueError("GOOGLE_API_KEY not found in environment or .env file")
+
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+
+# Set up the model
+vision_generation_config = {
+    "temperature": 0.4,
+    "top_p": 1,
+    "top_k": 32,
+    "max_output_tokens": 4096,
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+]
+
+vision_model = genai.GenerativeModel(
+    model_name="gemini-1.0-pro-vision-latest",
+    generation_config=vision_generation_config,
+    safety_settings=safety_settings,
+)
+
+def read_text_from_pdf(pdf_path):
+    text = ""
+    pdf_document = fitz.open(pdf_path)
+    for page_number in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_number)
+        text += page.get_text()
+    return text
+
+def read_text_from_image(file):
+    
+    image_data = file.read()
+    
+    image_parts = [
+        {"mime_type": "image/jpeg", "data": image_data},
+    ]
+    
+    prompt = [
+        """
+        Read all the text from the image provided and return it as a string.
+        It should be readable by a human.
+        If "\n" is present, replace it by creating a new line.
+        If "\t" is present, replace it by creating a tab.
+        """,
+        image_parts[0],
+    ]
+    
+    response = vision_model.generate_content(prompt)
+    print(response.text)
+    
+    return response.text
+
+# Example usage
+pdf_path = "example.pdf"  # Path to your PDF file
+image_path = "example.png"  # Path to your image file
+
+# Reading text from PDF
+pdf_text = read_text_from_pdf(pdf_path)
+print("Text from PDF:")
+print(pdf_text)
+
+# Reading text from image
+image_text = read_text_from_image(image_path)
+print("\nText from image:")
+print(image_text)
