e753ef852d44d94fcce37df9e0a8b58616b2897b
Shreya Shah <shreyashah100803@gmail.com>
2024-03-30 12:16:21 +0530

    added task type

--- shreya/classify_task.py
+++ shreya/classify_task.py
@@ -38,8 +38,8 @@ def chat():
         f"""
           Given a raw text input to a language model select the task best suited for the input, depending on what the user seems to need: 
           if the user asks for any mathematical calculations that require a calculator (example: addition, subtraction, multiplication, division, sin, cosine, standard deviation, etc), 
-            identify task_type based on context: "new event", "delete event", "check events", "edit event", "check date"
-          answer format: 'calculator', task_type
+            identify operators and operation
+          answer format: 'calculator', operators, operation
           
           if the user asks to execute a command prompt or a code:
           answer format: 'command_execute', input_string
@@ -47,13 +47,26 @@ def chat():
           if the user asks to send an email:
             identify task_type: 'send', 'delete', 'check_inbox'
             in case of task_type 'send', identify the message
-          answer format:'email' , task_type, message (if any)
+          answer format:'email', task_type, message (if any)
           
-          'message' if the user asks to send a text message
-          'api' if user asks for current news, sports events or the weather
-          'calendar' if user asks to edit their schedule (example: add or delete tasks for a day or hour, etc)
-          'web' if user specifically asks to search the web for something
-          'chat' if none of the above tasks apply
+          if the user asks to send a text message:
+            identify the message and the receiver
+          answer format: 'text', message, receiver
+          
+          if user asks for current news, sports events or the weather:
+            identify api_type: 'news', 'sports', 'weather'
+          answer format: 'api', api_type
+          
+          if user asks to edit their schedule (example: add or delete tasks for a day or hour, etc):
+           identify task_type based on context: "new event", "delete event", "check events", "edit event", "check date" 
+          answer format: 'calendar' 
+          
+          if user specifically asks to search the web for something:
+            identify prompt to search
+          answer format: 'web', prompt
+          
+          if none of the above tasks apply:
+          answer format: 'chat', user input
           
           given user input: {input_string}
           """
@@ -62,28 +75,28 @@ def chat():
     response = model.generate_content(classification_prompt)
 
     if "calculator" in response.text:
-        print("calculator")
+        print(response.text)
 
     elif "command" in response.text:
-        print("command_execute")
+        print(response.text)
         
     elif "email" in response.text:
-        print("email")
+        print(response.text)
         
-    elif "message" in response.text:
-        print("message")
+    elif "text" in response.text:
+        print(response.text)
         
     elif "api" in response.text:
-        print("api")
+        print(response.text)
         
     elif "calendar" in response.text:
-        print("calendar")
+        print(response.text)
     
     elif "web" in response.text:
-        print("web")
+        print(response.text)
         
     elif "chat" in response.text:
-        print("chat")
+        print(response.text)
 
     else:
         return "Invalid response"
